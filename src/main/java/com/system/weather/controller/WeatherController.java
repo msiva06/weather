5688aa647cce986f97be18c250128a988ae36fa7
msiva06 <m.sivasubramanian06@gmail.com>
2024-10-16 20:08:40 -0500

    Implemented error handling logic for invalid zipcode

--- src/main/java/com/system/weather/controller/WeatherController.java
+++ src/main/java/com/system/weather/controller/WeatherController.java
@@ -1,5 +1,6 @@
 package com.system.weather.controller;
 
+import com.system.weather.exception.ZipCodeNotFoundException;
 import com.system.weather.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,14 @@ public class WeatherController {
 
     @GetMapping("/weather/{zipcode}")
     public float getTemperature(@PathVariable String zipcode){
+        if(zipcode.isEmpty() || zipcode.trim().length() == 0 || zipcode.toLowerCase().equals("null")){
+            throw new ZipCodeNotFoundException("Nothing to geocode");
+        }
+        for(int i = 0; i < zipcode.length(); i++) {
+            if (!Character.isDigit(zipcode.charAt(i))) {
+                throw new ZipCodeNotFoundException("Invalid zip code");
+            }
+        }
         return service.getTemperature(zipcode);
     }
 }
